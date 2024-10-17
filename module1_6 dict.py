8ad350cd12af55d4f0a9d4075eb4385bc0c83f57
Sat9995 <saidboy995@gmail.com>
2024-10-17 11:25:53 -0700

    Add files via upload

---
+++ module1_6 dict.py
@@ -0,0 +1,11 @@
+name = 'my_dict'
+my_dict= {'Sat':1993}
+print(my_dict)
+print(my_dict['Sat'])
+print(my_dict.get('Leon'))
+my_dict.update({'Leon': 1991, 'Hail': 1971})
+a = my_dict.pop('Hail')
+print(my_dict)
+
+
+
